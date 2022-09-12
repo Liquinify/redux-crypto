5644153bb4f5fefa56f39fbd5eb8d37d142333c2
Arian <6986714@stud.nau.edu.ua>
2022-09-12 21:17:06 +0300

    Removing some stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import useLocalStorage from 'use-local-storage';
+import axios from 'axios';
+import { Routes, Route } from 'react-router-dom';
+import './scss/main.scss';
+import Crypto from './components/Crypto';
+import Navbar from './components/Navbar';
+import CryptoDescription from './pages/CryptoDescription';
 
-function App() {
+const App = () => {
+
+  const [crypto, setCrypto] = useState([])
+  const [page, setPage] = useState(1)
+  const [theme, setTheme] = useLocalStorage('theme' ? 'dark' : 'light')
+ 
+  useEffect(() => {
+    axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=${page}&sparkline=false`).then(response => {
+      setCrypto(crypto => [...crypto, ...response.data])
+    }).catch(error => {
+      console.log(error)
+    })
+  }, [page])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" data-theme={theme}>
+      <Navbar theme={theme} setTheme={setTheme}/>
+      <Routes>
+        <Route path='/' element={<Crypto crypto={crypto} setCrypto={setCrypto} setPage={setPage}/>} />
+        <Route path='/coin' element={<CryptoDescription />}>
+          <Route path=':coinId' element={<Crypto />}/>
+        </Route>
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
